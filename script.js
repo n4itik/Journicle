3206cada8ff464e9466c21532fd7bcce13a9091e
Naitik Yadav <naitikyadav1307@gmail.com>
2023-08-04 20:02:57 +0530

    Change navigation method

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ import { postsArray } from "./data.js";
 // Home
 const title = document.querySelector(".title");
 title.addEventListener("click", () => {
-  window.location.replace("index.html");
+  window.location.href = "index.html";
 });
 
 // Featured Post
 const featured = document.querySelector(".featured");
 if (featured) {
   featured.addEventListener("click", () => {
-    window.location.replace("featured.html");
+    window.location.href = "featured.html";
   });
 }
 
